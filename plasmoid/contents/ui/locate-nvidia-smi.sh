22a7e4a2797d38825e2b2a6de33d59a33ebd201b
bxabi <csabi@bxabi.com>
2018-04-27 16:58:22 +0200

    find the nvidia-smi tool also in lib64/nvidia-current (PCLinuxOS)

--- plasmoid/contents/ui/locate-nvidia-smi.sh
+++ plasmoid/contents/ui/locate-nvidia-smi.sh
@@ -3,17 +3,22 @@ which nvidia-smi >/dev/null 2>&1
 if [ $? -eq 0 ] ; then
     nvidia-smi $@
 else 
-   version=`modinfo nvidia | grep ^version: |grep -o "[0-9]*\.[0-9]*"`
-   tryUsrLib=/usr/lib/nvidia-$version/bin/nvidia-smi
-   if [ -f $tryUsrLib ] ; then
+  tryUsrLib=/usr/lib64/nvidia-current/bin/nvidia-smi #PPCLinuxOS
+  if [ -f $tryUsrLib ] ; then
+     $tryUsrLib $@
+  else   
+    version=`modinfo nvidia | grep ^version: |grep -o "[0-9]*\.[0-9]*"`
+    tryUsrLib=/usr/lib/nvidia-$version/bin/nvidia-smi
+    if [ -f $tryUsrLib ] ; then
       $tryUsrLib $@
-   else   
+    else   
       echo "/usr/bin/nvidia-smi not found"
       echo "$tryUsrLib not found"
       echo "Could not find the nvidia-smi utility in your system."
       echo "Do you have the nvidia-bumblebee package installed?"
-      echo "If yes, please send the path to it to csabi@bxabi.com"
+      echo "If yes, please send the path of your nvidia-smi to csabi@bxabi.com"
       echo "As a workaround you can add it to the PATH"
-      echo "or create a symlink accessible from the PATH"
-   fi
+      echo "or create a symlink to it which is accessible from the PATH"
+    fi
+  fi
 fi
